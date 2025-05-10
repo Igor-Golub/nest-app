fb2668146122fdb6ac20d09800244d98eeab63d8
igor-golub <gitihonovich@gmail.com>
2025-05-10 10:13:32 +0300

    feat(migrations): Added config path.

--- src/core/dbOptions.ts
+++ src/core/dbOptions.ts
@@ -1,12 +1,10 @@
 import { config } from 'dotenv';
 import { DataSourceOptions } from 'typeorm';
 
-config();
+config({ path: '.env.development' });
 
 export const dbOptions: DataSourceOptions = {
   type: 'postgres',
   synchronize: false,
-  url:
-    process.env.POSTGRES_DB_URL ??
-    'postgresql://postgres:postgres@localhost:3334',
+  url: process.env.POSTGRES_DB_URL,
 };
